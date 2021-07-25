80997c9d69f9527b4b96c40175d5205b8b67f026
monpro <monproshen@gmail.com>
2021-07-25 14:23:53 +1000

    refactor id generator class by adding interface and extracting methods

---
+++ src/main/java/com/monpro/designpattern/idgenerator/LogTraceIdGenerator.java
@@ -0,0 +1,4 @@
+package com.monpro.designpattern.idgenerator;
+
+public interface LogTraceIdGenerator extends IdGenerator {
+}
---
+++ src/main/java/com/monpro/designpattern/idgenerator/RandomIdGenerator.java
@@ -0,0 +1,48 @@
+package com.monpro.designpattern.idgenerator;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+import java.util.Random;
+
+@Slf4j
+public class RandomIdGenerator implements LogTraceIdGenerator {
+
+  @Override
+  public String generate() {
+    final String localHostName = getLastFieldOfHostName().orElse("Default Host");
+    final String randomString = generateRandomAlpha(8);
+    return String.format("%s-%d-%s", localHostName, System.currentTimeMillis(), randomString);
+  }
+
+  private Optional<String> getLastFieldOfHostName() {
+    try {
+      final String localHostName = InetAddress.getLocalHost().getHostName();
+      final String[] tokens = localHostName.split("\\.");
+      return Optional.of(tokens[tokens.length - 1]);
+    } catch (UnknownHostException e) {
+      log.error("Unable to get hostName");
+    }
+    return Optional.empty();
+  }
+
+  private String generateRandomAlpha(final int length) {
+    final char[] randomChar = new char[length];
+    final int maxAsc = 'z';
+    int count = 0;
+    Random random = new Random();
+    while (count < length) {
+      final int randomAsc = random.nextInt(maxAsc);
+      final boolean isDigit = randomAsc >= '0' && randomAsc <= '9';
+      final boolean idUpperCase = randomAsc >= 'A' && randomAsc <= 'Z';
+      final boolean isLowerCase = randomAsc >= 'a';
+      if (isDigit || idUpperCase || isLowerCase) {
+        randomChar[count] = (char) (randomAsc);
+        count += 1;
+      }
+    }
+    return String.valueOf(randomChar);
+  }
+}
