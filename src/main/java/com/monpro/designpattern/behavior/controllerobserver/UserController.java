e9e61792b8d3dba2f47c0bd290d409c583af432c
monpro <monproshen@gmail.com>
2021-10-17 13:01:54 +1100

    add ExecutorService to make registerObserver.handleRegisterSuccess non-blocking

--- src/main/java/com/monpro/designpattern/behavior/controllerobserver/UserController.java
+++ src/main/java/com/monpro/designpattern/behavior/controllerobserver/UserController.java
@@ -1,11 +1,17 @@
 package com.monpro.designpattern.behavior.controllerobserver;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+@Slf4j
 public class UserController {
 
   private final List<RegisterObserver> registerObservers = new ArrayList<>();
+  final ExecutorService executor = Executors.newFixedThreadPool(10);
 
   public void setObservers(final List<RegisterObserver> observers) {
     registerObservers.addAll(observers);
@@ -13,7 +19,12 @@ public class UserController {
 
   public Long register(final String userName) {
     // UserId is always 1 for demo
-    registerObservers.forEach(registerObserver -> registerObserver.handleRegisterSuccess(1));
+    registerObservers.forEach(
+        registerObserver -> executor.execute(
+            () -> {
+              log.info("Current Thread Id: {}", Thread.currentThread().getId());
+              registerObserver.handleRegisterSuccess(1);
+            }));
     return 1L;
   }
 }
