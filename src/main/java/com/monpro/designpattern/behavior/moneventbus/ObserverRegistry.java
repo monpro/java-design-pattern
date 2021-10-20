a86fcb47d6a4d84b054987975380969557d13315
monpro <monproshen@gmail.com>
2021-10-20 22:45:37 +1100

    add findMatchedObservedActions and registry

--- src/main/java/com/monpro/designpattern/behavior/moneventbus/ObserverRegistry.java
+++ src/main/java/com/monpro/designpattern/behavior/moneventbus/ObserverRegistry.java
@@ -38,7 +38,7 @@ public class ObserverRegistry {
    * @param observer A observer instance.
    * @return A map from eventType to ObserverAction.
    */
-  private Map<Class<?>, Collection<ObserverAction>> findObserverActions(final Observer observer) {
+  private Map<Class<?>, Collection<ObserverAction>> findObserverActions(final Object observer) {
     final Map<Class<?>, Collection<ObserverAction>> observerActions = new HashMap<>();
     final Class<?> clazz = observer.getClass();
     findAnnotatedMethods(clazz).forEach(method -> {
@@ -48,5 +48,32 @@ public class ObserverRegistry {
       observerActions.get(eventType).add(new ObserverAction(observer, method));
     });
     return observerActions;
+  }
+
+  public List<ObserverAction> findMatchedObservedActions(final Object event) {
+    final List<ObserverAction> matchedObservedActions = new ArrayList<>();
+    final Class<?> eventType = event.getClass();
+    for(final Map.Entry<Class<?>, CopyOnWriteArraySet<ObserverAction>> entry: registry.entrySet()) {
+      final Class<?> entryEventType = entry.getKey();
+      final CopyOnWriteArraySet<ObserverAction> entryActions = entry.getValue();
+      if(entryEventType.isAssignableFrom(entryEventType)) {
+        matchedObservedActions.addAll(entryActions);
+      }
+    }
+    return matchedObservedActions;
+  }
+
+  public void register(final Object observer) {
+    final Map<Class<?>, Collection<ObserverAction>> observerActions = findObserverActions(observer);
+    for( Map.Entry<Class<?>, Collection<ObserverAction>> entry : observerActions.entrySet()) {
+      final Class<?> entryEventType = entry.getKey();
+      final Collection<ObserverAction> entryActions = entry.getValue();
+      CopyOnWriteArraySet<ObserverAction> registeredEventActions = registry.get(entryEventType);
+      if (registeredEventActions == null) {
+        registeredEventActions = new CopyOnWriteArraySet<>();
+      }
+      registeredEventActions.addAll(entryActions);
+      registry.put(entryEventType, registeredEventActions);
+    }
   }
 }
